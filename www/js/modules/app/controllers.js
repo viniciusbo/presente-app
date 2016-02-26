85346526b33defb763b2e90bfaa7d7c4c28fe09a
Vinícius Borriello <viniciusbo@gmail.com>
2016-02-26 00:09:34 -0300

    Implement configurable attendance count and time ago on pending attendance list

--- www/js/modules/app/controllers.js
+++ www/js/modules/app/controllers.js
@@ -1,7 +1,7 @@
 angular.module('app.controllers', ['course.service'])
   .controller('AppCtrl', AppCtrl);
 
-function AppCtrl($rootScope, $scope, $state, $ionicModal, $q, courseService) {
+function AppCtrl($rootScope, $scope, $state, $ionicModal, $ionicPopover, $q, courseService) {
   var displayAttendanceList = function() {
     courseService
       .getAttendanceListByDate()
@@ -25,12 +25,21 @@ function AppCtrl($rootScope, $scope, $state, $ionicModal, $q, courseService) {
   displayAttendanceList();
   displayPendingAttendanceCount();
 
+  $scope.moment = moment;
+
   $scope.pendingAttendanceCount = 0;
 
   $scope.$on('courses:updated', function() {
     displayPendingAttendanceCount();
     displayAttendanceList();
   });
+
+  $scope.openNewClassModal = function() {
+    initModal()
+      .then(function() {
+        $scope.modal.show();
+      })
+  };
 
   var initModal = function() {
     if ($scope.modal) {
@@ -47,13 +56,6 @@ function AppCtrl($rootScope, $scope, $state, $ionicModal, $q, courseService) {
     }
   };
 
-  $scope.openNewClassModal = function() {
-    initModal()
-      .then(function() {
-        $scope.modal.show();
-      })
-  };
-
   $scope.closeNewClassModal = function() {
     $scope
       .modal
@@ -63,23 +65,30 @@ function AppCtrl($rootScope, $scope, $state, $ionicModal, $q, courseService) {
       });
   };
 
-  $scope.didAttend = function(courseId, date) {
+  $scope.openAttendancePopover = function($event, attendance) {
+    $scope.attendance = attendance;
+    $scope.maxAttendanceCount = attendance.count;
+    $scope.popover.show($event);
+  };
+
+  $scope.didAttend = function(attendance) {
     courseService
-      .didAttend(courseId, date)
+      .didAttend(attendance.courseId, attendance.date, attendance.count)
       .then(function(result) {
         displayAttendanceList();
-          displayAttendanceList();
-          displayPendingAttendanceCount();
-          $rootScope.$broadcast('attendance:updated');
-          $state.go('app.courses_main');
+        displayPendingAttendanceCount();
+        $rootScope.$broadcast('attendance:updated');
+        $state.go('app.courses_main');
       }, function(err) {
         console.error(err);
       });
+
+      $scope.popover.hide();
   };
 
-  $scope.didNotAttend = function(courseId, date) {
+  $scope.didNotAttend = function(attendance) {
     courseService
-      .didNotAttend(courseId, date)
+      .didNotAttend(attendance.courseId, attendance.date)
       .then(function(result) {
         displayAttendanceList();
         displayPendingAttendanceCount();
@@ -89,4 +98,12 @@ function AppCtrl($rootScope, $scope, $state, $ionicModal, $q, courseService) {
         console.error(err);
       });
   };
+
+  $ionicPopover
+    .fromTemplateUrl('js/modules/course/templates/did-attend-popover.html', {
+      scope: $scope
+    })
+    .then(function(popover) {
+      $scope.popover = popover;
+    });
 }
